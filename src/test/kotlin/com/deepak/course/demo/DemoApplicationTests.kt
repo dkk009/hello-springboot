0ae255094786cb8f893b05c9687303dd62ba47d7
Deepak KK <dkk0011@gmail.com>
2023-04-19 17:08:28 +0530

    Spring-boot project setup

---
+++ src/test/kotlin/com/deepak/course/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.deepak.course.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
